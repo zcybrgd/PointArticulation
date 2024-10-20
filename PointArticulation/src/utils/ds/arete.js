336c7cc1c2fff0f2a0fd160b8482a8295389655a
zcybrgd <zainebb7k9h1@gmail.com>
2024-10-20 16:30:16 +0100

    init basic classes and their functions

---
+++ PointArticulation/src/utils/ds/arete.js
@@ -0,0 +1,7 @@
+class Arete {
+    constructor(id,nodeEx1,nodeEx2){
+        this.id = id
+        this.nodeEx1 = nodeEx1
+        this.nodeEx2 = nodeEx2
+    }
+}
---
+++ PointArticulation/src/utils/ds/graphe.js
@@ -0,0 +1,60 @@
+// le graphe est non orientée
+class Graphe {
+    constructor(){
+        this.noeuds = new Map()
+        this.aretes = new Set() // pour qu'il y est pas une duplication ou quoi que ce soit
+    }
+
+    ajouterNoeud(id,name){
+        if(!this.noeuds.has(id)){
+            this.noeuds.set(id, new Sommet(id,name))
+        }
+    }
+
+    supprimerNoeud(id){
+        this.noeuds.delete(id)
+        this.aretes.forEach(e => {
+            if(e.nodeEx1 == id || e.nodeEx2 == id){
+                this.aretes.delete(e)
+            }
+        })
+    }
+
+    ajouterArete(id,nodeEx1id,nodeEx2id){
+        if(this.noeuds.has(nodeEx1id) && this.noeuds.has(nodeEx2id)){
+            const nouvelleArete = new Arete(id, nodeEx1id, nodeEx2id)
+            this.aretes.add(nouvelleArete)
+        }
+    }
+
+    supprimerArete(id){
+        this.aretes.forEach(e => {
+            if(e.id==id){
+                this.aretes.delete(e)
+            }
+        });
+    }
+    
+    // va trouver les noeuds adjacents d'un noeud dans le graphe
+    // to be optimized later if icould (i want it to map directly le noeud and not loop over toutes les aretes du graphe)
+    trouverVoisins(noeudId){
+        const voisins = []
+        this.aretes.forEach(
+            e => {
+                if(e.nodeEx1 == noeudId) voisins.push(e.nodeEx2)
+                else if (e.nodeEx2 == noeudId) voisins.push(e.nodeEx1)
+            }
+        )
+    }
+    
+    // on va ensuite l'utiliser pendant la suppression d'un sommer
+    copier(){
+      const copieGraphe = new Graphe()
+      this.noeuds.forEach((id,n) => {
+          copieGraphe.ajouterNoeud(id, new Sommet(id,n.label))
+      } )
+      this.aretes.forEach((e) => {
+        copieGraphe.ajouterArete(new Arete(e.id,e.nodeEx1,e.nodeEx2))
+      })
+    }
+}
